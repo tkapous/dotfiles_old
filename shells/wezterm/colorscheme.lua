4c0720154e2df713e43c92a78ee720343fe674f5
biggybi <biggybi@pm.me>
2025-03-20 06:39:51 +0100

    feat(wezterm): colorscheme: default theme (day)

--- shells/wezterm/colorscheme.lua
+++ shells/wezterm/colorscheme.lua
@@ -1,16 +1,17 @@
 local wezterm = require("wezterm")
 
 local themes = {
+	Default = "tokyonight_day",
 	Light = "tokyonight_day",
-	Dark = "tokyonight_moon",
+	Dark = "tokyonight_dark_moon",
 	LightHighContrast = "tokyonight_day",
-	DarkHighContrast = "tokyonight_moon",
+	DarkHighContrast = "tokyonight_dark_moon",
 }
 
 local function change_theme(window)
 	local overrides = window:get_config_overrides() or {}
 	local appearance = window:get_appearance()
-	local scheme = themes[appearance] or "tokyonight_moon"
+	local scheme = themes[appearance] or themes.default
 	if overrides.color_scheme ~= scheme then
 		overrides.color_scheme = scheme
 		window:set_config_overrides(overrides)
