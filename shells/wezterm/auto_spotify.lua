10390b03ebb84ab64a2f86a5c467ded8def065fd
biggybi <biggybi@pm.me>
2025-03-22 11:02:41 +0100

    feat(wezterm): auto_spotify

---
+++ shells/wezterm/auto_spotify.lua
@@ -0,0 +1,55 @@
+local wezterm = require("wezterm")
+local io = require("io")
+
+local function is_spotify_running()
+	local handle = io.popen("pgrep spotify")
+	if not handle then
+		return false
+	end
+	local result = handle:read("*a")
+	handle:close()
+	return result ~= ""
+end
+
+local function is_spotify_playing()
+	local handle = io.popen("playerctl -p spotify status")
+	if not handle then
+		return false
+	end
+	local result = handle:read("*a")
+	handle:close()
+	return result:match("Playing")
+end
+
+local function is_any_sound_playing()
+	local players_handle = io.popen("playerctl -l")
+	if not players_handle then
+		return false
+	end
+	wezterm.log_warn("Checking for any sound playing")
+	for player in players_handle:lines() do
+		wezterm.log_warn("|" .. player .. "|")
+		local handle = io.popen("playerctl -p " .. player .. " status")
+		if not handle then
+			return false
+		end
+		local result = handle:read("*a")
+		wezterm.log_warn(result)
+		handle:close()
+		if result:match("Playing") then
+			return true
+		end
+	end
+end
+
+local function play_spotify()
+	os.execute("playerctl -p spotify play")
+end
+
+wezterm.on("window-focus-changed", function(window)
+	return window:is_focused()
+		and is_spotify_running()
+		and not is_spotify_playing()
+		and not is_any_sound_playing()
+		and play_spotify()
+end)
--- shells/wezterm/wezterm.lua
+++ shells/wezterm/wezterm.lua
@@ -2,5 +2,6 @@ local config = require("config")
 
 require("opacity")
 require("colorscheme")
+require("auto_spotify")
 
 return config
